7935b5ee2c15a97d272b4223c4230c7b44f6f0f9
Akash Dev <epatchdev1234@gmail.com>
2016-01-09 02:04:37 +0530

    added logout and check functionality

--- src/test/java/com/dev/GmailTest/GmailTest.java
+++ src/test/java/com/dev/GmailTest/GmailTest.java
@@ -31,11 +31,16 @@ public class GmailTest {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.findElement(By.xpath(".//*[@id='Passwd']")).sendKeys("epatchdev1234");
 		driver.findElement(By.id("signIn")).click();
-
 		// verify login
 		WebDriverWait wait = new WebDriverWait(driver, 3000);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//span[@class='gb_Nb']")));
 		Assert.assertTrue(driver.findElements(By.partialLinkText("Inbox")).size() > 0, "Inbox Should exist");
+		
+		driver.findElement(By.xpath("//span[@class='gb_Za gbii']")).click();
+		driver.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
+		driver.findElement(By.xpath(".//*[@id='gb_71']")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='logo logo-w']")));
+		Assert.assertTrue(driver.findElements(By.partialLinkText("Need help?")).size() > 0, "Need help should appear");
 	}
 
 	@AfterTest
